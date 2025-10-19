9e3b6dfa68c4a2a7c03d4a0107b2a4f46a991a1b
Ismail Bin Mujeeb <IsmailBinMujeeb@gmail.com>
2025-10-19 09:30:07 +0530

    chore(server): console log enahnced in on listen for production

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 import app from "./server.js";
 import db from "./config/db.js";
+import env from "./config/env.js";
 
-; (
+(async function () {
+  const PORT = app.get("port");
 
-    async function () {
-        const PORT = app.get('port');
-
-        await db();
-        app.listen(PORT, () => {
-            console.log(`Server is running at http://localhost:${PORT}/api/health`)
-        })
+  await db();
+  app.listen(PORT, () => {
+    if (env.IS_DEV) {
+      console.log(`Server is running at http://localhost:${PORT}/api/health`);
+    } else {
+      console.log(`Server is running at ${env.SERVER_URL}/api/health`);
     }
-)()
+  });
+})();
